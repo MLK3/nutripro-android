6dc1f4f5921a6f286fd2d2590f16dda539e0cef3
Filippe Carvalho <filippe.cl@gmail.com>
2016-10-25 20:08:08 -0200

    login layout fixes

--- app/src/main/java/com/oddsix/nutripro/BaseApplication.java
+++ app/src/main/java/com/oddsix/nutripro/BaseApplication.java
@@ -2,8 +2,10 @@ package com.oddsix.nutripro;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.oddsix.nutripro.utils.Constants;
 
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -16,7 +18,15 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        RealmConfiguration config = new RealmConfiguration.Builder(this).name(Constants.BASE_DB_PATH)
+        Fabric.with(this, new Crashlytics());
+        RealmConfiguration config = new RealmConfiguration.Builder(this)
+                .name(Constants.BASE_DB_PATH)
+                .initialData(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        
+                    }
+                })
                 .deleteRealmIfMigrationNeeded().build();
         Realm.setDefaultConfiguration(config);
     }
